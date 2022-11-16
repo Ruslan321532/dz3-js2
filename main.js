84ff5c910902a48daed2b7712f22219e081a36be
ruslan <ruslanburalkiev8@gmail.com>
2022-11-16 16:50:21 +0600

    rusya

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 const menu = document.querySelector('.menu')
-
-fetch('https://api.escuelajs.co/api/v1/products/')
+fetch(`https://fakestoreapi.com/products`)
 .then((res) => res.json())
 .then((res) => {
-res.forEach((item) => {
-menu.innerHTML += `
-<div class="card">
-<img class="card_img" src='${item.images}' alt="">
-<p class="card_title">${item.title}</p>
-<p class="card_title">${item.price}</p>
-<p class="card_description">${item.description}</p>
-</div>
-`
+    res.forEach((item) => {
+        menu.innerHTML += `
+            <div class="all__content">
+            <img src="${item.image}" alt="">
+            <h2 class = 'card_title'>${item.title}</h2>
+            <p class ="card__category">Категория: ${item.category}</p>
+            <p class ="card__price">цена: ${item.price}</p>
+            <p class ="card__rating">Рейтинг: ${item.rating.rate}</p>
+            </div>
+        `
+    })
 })
-})
+menu('all')
